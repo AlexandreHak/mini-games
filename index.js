5245b8701933570aa386fa43f30bad48de1d41f1
alexandre <alexandre.hak@epitech.eu>
2018-06-29 20:01:47 +0200

    Initial tic-tac-toe

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const app = express();
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/routes/index.html');
 });
 
 app.get('/palindrome', (req, res) => {
-  res.sendFile(__dirname + '/palindrome.html');
+  res.sendFile(__dirname + '/routes/palindrome.html');
 });
 
 app.get('/hangman', (req, res) => {
-  res.sendFile(__dirname + '/hangman.html');
+  res.sendFile(__dirname + '/routes/hangman.html');
+});
+
+app.get('/tic-tac-toe', (req, res) => {
+  res.sendFile(__dirname + '/routes/tic-tac-toe.html');
 });
 
 const port = process.env.PORT || 3000;
